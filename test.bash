bfd4f2a29881773183caff357659b520dc0bdd26
eisuk <s21c1123ix@s.chibakoudai.jp>
2022-11-03 13:36:43 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -3,15 +3,15 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-      echo NG at Line $1
-      res=1
+        echo NG at Line $1
+	ews=1
 }
-
+	    　
 res=0
-
-
+	    　
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 14 ] || ng ${LINENO}
-
+	    　
 [ "$res" = 0 ] && echo OK        
 exit $res
