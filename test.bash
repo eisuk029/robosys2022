bae118e6c26907139bdfe50878a9b212cf7d6831
eisuk <s21c1123ix@s.chibakoudai.jp>
2022-11-27 06:58:23 +0900

    Rename program

--- test.bash
+++ test.bash
@@ -3,15 +3,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-        echo NG at Line $1
+        echo ${1}行目が違うよ
 	ews=1
 }
-	    　
+
 res=0
-	    　
+
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14 ] || ng ${LINENO}
-	    　
-[ "$res" = 0 ] && echo OK        
+[ "${out}" = 15 ] || ng "${LINENO}"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "${LINENO}"
+[ "${out}" = "" ] || ng "${LINENO}"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
